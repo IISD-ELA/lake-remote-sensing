4388e5f0acfa49aba99aa17f8408d0f7f516849e
ge0gunn <ggunn@iisd.ca>
2020-11-02 12:34:58 -0600

    removed depricated lines.

--- GEE_IISD-ELA_L8_L227.js
+++ GEE_IISD-ELA_L8_L227.js
@@ -1,8 +1,7 @@
 // Goal - Get landsat histograms for all dates within custom ROI.
 /* Steps
-*  1. Get lake offshore geometry (in this case a 3.5 m isobat from MBGov)
-*  2. Get dates of interest (ice free season)
 */
+//need a var geometry = ...
 
 var date_0 = '2011-01-01';
 var date_1 = '2020-11-02';
@@ -13,20 +12,7 @@ var collection = ee.ImageCollection('LANDSAT/LC08/C01/T1_SR')
 //var collection = ee.ImageCollection('LANDSAT/LC8_SR').filterDate(ee.Date('2015-01-01'),ee.Date('2015-12-31'));
 
 var L227 = geometry;
-//var PellyNonHarvested = geometry2;
 
-// var ELAsubwatersheds = ee.FeatureCollection('ft:16RfV0Igg4HyPZoIVYKWvbyL1tZ7GVdQvNNY2vZLN');
-
-// selection of ImageCollection is key.  Some collections that I thought were processed to SR
-// Aren't actually, so I need to ensure the numbers are coming out as reflectances.  
-// Potential Collections 'LANDSAT/LC8_L1T_TOA' , 'LANDSAT/LC8_SR'
-//var landsat8_B5 = ee.ImageCollection('LANDSAT/LC8_SR')
-//    .filterDate('2013-05-10', '2013-08-30')
-//    .select('B[5]');
-
-//var landsat8_B4 =ee.ImageCollection('LANDSAT/LC8_SR')
-//filterDate('2015-05-10', '2015-08-30')
-//    .select('B[4]');
     
 var landsat8QA = ee.ImageCollection('LANDSAT/LC08/C01/T1_SR')
     .filterDate(ee.Date(date_0),ee.Date(date_1))
@@ -38,18 +24,10 @@ var LakeTimeSeries = ui.Chart.image.series(collection, geometry, ee.Reducer.mean
 print(LakeTimeSeries);
 var LakeTimeQA = ui.Chart.image.series(landsat8QA, geometry, ee.Reducer.mode(), 30);
 print(LakeTimeQA);
-//print("ELA_subwatersheds");
-//print(Chart.image.series(collection, ELAsubwatersheds, ee.Reducer.median(), 30));
-//print(Chart.image.histogram(
-//print(Chart.image.series(landsat8QA, ELAsubwatersheds, ee.Reducer.mode(), 30));
-
-// insert QA test here to remove clouds (and cloud shadows) and sun glint, and ...
-//var landsat8SRstat = landsat8SR.mean();
-
-// Make a feature without geometry and set the properties to the dictionary of means.
 
 
-// Export Section
+
+// Export Section, not operational right now.
 /*
 var feature = ee.Feature(null, means);
 
@@ -63,9 +41,8 @@ Export.table.toDrive({
   fileFormat: 'CSV'
 });
 */
-//Map.addLayer(ELAsubwatersheds);
 
-//Map.setCenter(-93.722934, 49.66210, 15);
+Map.setCenter(-93.722934, 49.66210, 15);
 
 var l8 = ee.ImageCollection('LANDSAT/LC08/C01/T1_TOA');
 // This will sort from least to most cloudy.
